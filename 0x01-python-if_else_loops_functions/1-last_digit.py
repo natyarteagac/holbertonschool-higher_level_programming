2c248ee23b18ea3bcd947974b0ac32a5d2031d55
natyarteagac <natalia.arteagac94@gmail.com>
2021-04-27 22:05:25 +0000

    Indentation with pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	last_number = number % -10
+    last_number = number % -10
 else:
-	last_number = number % 10
+    last_number = number % 10
 str = "Last digit of"
 if last_number > 5:
-	print("{0} {1} is {2} and is greater than 5".format(str, number, last_number))
+    print("{0} {1} is {2} and is greater than 5".format(str, number, last_number))
 elif last_number == 0:
-	print("{0} {1} is {2} and is 0".format(str, number, last_number))
+    print("{0} {1} is {2} and is 0".format(str, number, last_number))
 elif last_number < 6 and last_number != 0:
-	print("{0} {1} is {2} and is less than 6 and not 0".format(str, number, last_number))
+    print("{0} {1} is {2} and is less than 6 and not 0".format(str, number, last_number))
