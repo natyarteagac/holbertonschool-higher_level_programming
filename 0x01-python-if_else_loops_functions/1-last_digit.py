cda5e556e99d5a55d07b8ec160647ef1483685d6
natyarteagac <natalia.arteagac94@gmail.com>
2021-04-27 16:21:08 -0500

    changes with indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ str = "Last digit of"
 if last_number > 5:
     print("{0} {1} is {2} and is greater than 5".format(str, number, last_number))
 elif last_number == 0:
-   print("{0} {1} is {2} and is 0".format(str, number, last_number))
+    print("{0} {1} is {2} and is 0".format(str, number, last_number))
 elif last_number < 6 and last_number != 0:
-	print("{0} {1} is {2} and is less than 6 and not 0".format(str, number, last_number))
+    print("{0} {1} is {2} and is less than 6 and not 0".format(str, number, last_number))
