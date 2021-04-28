a3cf90b8d914a9183ae1a2ebd60bf0f2c8c86bcb
natyarteagac <natalia.arteagac94@gmail.com>
2021-04-28 19:03:40 +0000

    Add, sub, mul, div

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add
+    a = 10
+    b = 5
+
+    print("{} + {} = {}\n".format(a, b, add(a, b)), end='')
+
+    from calculator_1 import sub
+    a = 10
+    b = 5
+
+    print("{} - {} = {}\n".format(a, b, sub(a, b)), end='')
+
+    from calculator_1 import mul
+    a = 10
+    b = 5
+
+    print("{} * {} = {}\n".format(a, b, mul(a, b)), end='')
+
+    from calculator_1 import div
+    a = 10
+    b = 5
+
+    print("{} / {} = {}\n".format(a, b, div(a,b)), end='')
