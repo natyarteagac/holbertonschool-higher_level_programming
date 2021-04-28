c50e67523fe70fefbc7ae9ac272437148166483e
natyarteagac <natalia.arteagac94@gmail.com>
2021-04-28 19:45:45 +0000

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
 
     print(a, '+', b, '=', add(a, b), '\n', end='')
-
-    from calculator_1 import sub
-
     print(a, '-', b, '=', sub(a, b), '\n', end='')
-
-    from calculator_1 import mul
     print(a, '*', b, '=', mul(a, b), '\n', end='')
-
-    from calculator_1 import div
     print(a, '/', b, '=', div(a, b), '\n', end='')
