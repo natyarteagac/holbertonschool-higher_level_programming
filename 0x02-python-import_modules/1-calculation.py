b43b58b6fb5115f815f9846f7b1a1bc0f63c1073
natyarteagac <natalia.arteagac94@gmail.com>
2021-04-28 19:12:45 +0000

    Calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,19 +7,10 @@ if __name__ == "__main__":
     print("{} + {} = {}\n".format(a, b, add(a, b)), end='')
 
     from calculator_1 import sub
-    a = 10
-    b = 5
-
     print("{} - {} = {}\n".format(a, b, sub(a, b)), end='')
 
     from calculator_1 import mul
-    a = 10
-    b = 5
-
     print("{} * {} = {}\n".format(a, b, mul(a, b)), end='')
 
     from calculator_1 import div
-    a = 10
-    b = 5
-
     print("{} / {} = {}\n".format(a, b, div(a, b)), end='')
