e9f0cf05970700e130c7d5aeb2abc1a8aa70709b
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-06 05:28:38 +0000

    Printing matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for numberoflists in range(len(matrix)):
         for index in range(len(matrix[numberoflists])):
-            print("{}".format(matrix[numberoflists][index]), end='')
+            print("{:d}".format(matrix[numberoflists][index]), end='')
             if index + 1 < len(matrix[numberoflists]):
                 print(end=' ')
         print()
