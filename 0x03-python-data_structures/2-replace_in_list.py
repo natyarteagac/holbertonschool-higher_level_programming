8abb8d55db1cce03ba801663c557b09d9280dccc
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-06 01:32:05 +0000

    Replace the index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     elif idx + 1 > len(my_list):
         return my_list
-    elif idx + 1 < len(my_list):
-        my_list.remove(idx + 1)
-        my_list.insert(idx, element)
+    else:
+        my_list[idx] = element
+        return my_list
