5f68d2147a5192c1634cd18ca31ea8d96a08e14b
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-09 02:24:10 +0000

    Multiply the number

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
-    for v * 2 in sorted(a_dictionary.items()):
-        return new_dictionary
+    [value * 2 for value in new_dictionary]
+    return new_dictionary
