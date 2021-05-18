3dbf40eecca4588cde9fdb3386a6fe5ff007f054
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-18 16:50:48 -0500

    Veryfying if size if not an integer or less than zero

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+The class Square
+"""
+
+
+class Square:
+    """ Defining if size is not integer or less than 0 """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
