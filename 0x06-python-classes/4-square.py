e96d81a1871e4733b96d272387179e692693df3e
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-18 19:30:33 -0500

    Access and update private atribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+The class Square
+"""
+
+
+class Square:
+    """ Taking the area of size"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+
+    @property
+    def size(self):
+            return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        else:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = value
+
+    def area(self):
+        return self.__size ** 2
