607493fc220231bc0b37c87faead09aa7adae3f6
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-01 18:35:41 -0500

    Fixing comments

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
 """
     BaseGeometry
+
+
 """
 
 
 class BaseGeometry:
-    """ BaseGeometry class """
+    """ BaseGeometry class
+
+    """
 
     def area(self):
         raise Exception("area() is not implemented")
