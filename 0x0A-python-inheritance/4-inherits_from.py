e4431f7cfefa400984f315bf2096a6ae748d82a2
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-01 21:33:25 -0500

    Function to return True or False if the obj is subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,10 @@ a class that inherited
 
 
 def inherits_from(obj, a_class):
-    if issubclass(a_class):
+    """
+    Function to return the subclasses
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
