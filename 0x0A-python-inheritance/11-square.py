5fc7611693e676dd8c5e90feaad8d1ebac3835ab
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-01 22:05:19 -0500

    Function who creates the class Square and return the area

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    BaseGeometry class
+
+
+
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        return "{}".format(self.__size * self.__size)
+
+    def __str__(self):
+        str = '[Square] {}/{}'.format(self.__size, self.__size)
+        return str
