d1b88c0360dc7b93c53b646df1f0369d4a7773ef
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-03 11:58:27 -0500

    Correct output

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,5 +10,8 @@ class MyInt(int):
 
     """
 
-    def __bool__(self):
-        return False
+    def __eq__(self, x: object):
+        return super().__eq__(not x)
+
+    def __ne__(self, x: object):
+        return super().__ne__(not x)
