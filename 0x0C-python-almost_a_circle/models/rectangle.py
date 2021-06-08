6afbeed4227ae28cacbc6eefcc440b0cb5c34a1f
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-08 17:17:50 -0500

    Fixing checks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -123,8 +123,8 @@ class Rectangle(Base):
         """
         Returning a string
         """
-        return "[Rectangle]({}) {}/{} - {}/{}"\
-            .format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle]({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         """
