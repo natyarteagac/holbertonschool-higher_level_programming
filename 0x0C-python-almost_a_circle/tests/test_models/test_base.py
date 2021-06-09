d66c80b3907e3033ef39305a4105edcfed2935a3
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-09 14:27:12 -0500

    Starting with test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-
+"""
+    Creating Class Testing
+"""
+from models.base import Base
 import unittest
+import os
 
 
 class testing(unittest.TestCase):
-    pass
+    """
+    Creating Edge Cases for test
+    """
+
+    def testing_pep8(self):
+        """Testing pep8"""
+        os.popen("pep8 base.py").read()
