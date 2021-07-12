e250ccb50907324d02d3c274181ef0db19d13596
natyarteagac <natalia.arteagac94@gmail.com>
2021-07-12 17:01:54 -0500

    Couting the id's

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- Writing a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL servSer.
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
