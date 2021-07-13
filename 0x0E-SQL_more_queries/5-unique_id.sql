e80c2fbebb6663eabd5735f7944e44a276e7ab29
natyarteagac <natalia.arteagac94@gmail.com>
2021-07-13 18:42:04 -0500

    Unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- script that creates the table unique_id on your MySQL server.
 -- holberton school.
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT NULL UNIQUE, name VARCHAR(256)
+    id INT NULL DEFAULT(1) UNIQUE, name VARCHAR(256)
 );
