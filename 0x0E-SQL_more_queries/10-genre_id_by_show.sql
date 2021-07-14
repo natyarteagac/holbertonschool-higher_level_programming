2aa34686f2715e5f35e2ebdae31076861452cb6a
natyarteagac <natalia.arteagac94@gmail.com>
2021-07-13 23:14:01 -0500

    TV show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,2 @@
 -- Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-CREATE DATABASE hbtn_0d_tvshows
-USE hbtn_0d_tvshows
-curl "https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql"
-SELECT * FROM tv_shows.title, tv_show_genres.id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_shows.id - tv_show_genres.genre_id ON tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
