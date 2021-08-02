97f6fc600066a95fe14ea6727f2871d0915bb40d
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-02 16:41:41 -0500

    Printing with a for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Loop to print all the array
+*/
+
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < myVar.length; index++) {
+  console.log(myVar[index]);
+}
