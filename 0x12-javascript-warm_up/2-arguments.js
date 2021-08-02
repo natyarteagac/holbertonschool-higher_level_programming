f97262020a128a3abebdf4025145f7dac2166959
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-02 14:45:58 -0500

    Checking if arguments exists or not

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 /* Script to print if arguments exists or not
 */
+const myVar = process.argv.length;
 
-if (process.argv.length < 3) {
+if (myVar < 3) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (myVar === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
