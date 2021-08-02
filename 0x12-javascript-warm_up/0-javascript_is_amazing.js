0b185c95b07936b50b21074129112196b0d3b70b
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-02 12:14:38 -0500

    Starting with the project

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* Script to print the string JavaScript is amazing */
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Script to print the strings: C is fun, Python is cool and JavaScript is amazing */
+
+const myVarOne = 'C is fun'; const myVarTwo = 'Python is cool';
+const myVarThree = 'Javascript is amazing';
+console.log(myVarOne);
+console.log(myVarTwo);
+console.log(myVarThree);
