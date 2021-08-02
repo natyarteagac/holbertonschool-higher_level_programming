37ff4aa8743a265c77a02a42218b4f9557c1ad9f
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-02 14:43:20 -0500

    Fixing checkers

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/* Script to print if arguments exists or no
+/* Script to print if arguments exists or not
 */
 
-if (!process.argv[2]) {
+if (process.argv.length < 3) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
