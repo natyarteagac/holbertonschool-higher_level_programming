067b60a931505f2da217271406b1bbf6f575b209
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-02 14:31:08 -0500

    Fixing checks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 */
 
 if (!process.argv[2]) {
-  console.log('No argument')
-} else if (process.argv.length[3] === arguments.length[3]) {
-  console.log('Argument found')
+  console.log('No argument');
+} else if (process.argv.length[3] === process.argv[3]) {
+  console.log('Argument found');
 } else {
-  console.log('Arguments found')
+  console.log('Arguments found');
 }
