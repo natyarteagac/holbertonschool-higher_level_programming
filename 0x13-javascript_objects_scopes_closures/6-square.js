6987de79c9a778c1fbe15ab350ac842f64598e78
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-03 16:07:44 -0500

    Printing the Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* Define the Square class
+*/
+
+class Square extends require('./5-square') {
+  // Define Square class
+  constructor (size) {
+    // Calling rectangle's attributes.
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    // Print the size of Square and verify if variable exists print with the character C if not print with X
+    for (let index = 0; index < this.size; index++) {
+      if (!c) {
+        console.log('X'.repeat(this.size));
+      } else {
+        console.log(c.repeat(this.size));
+      }
+    }
+  }
+}
+module.exports = Square;
