e2d699d0bbf794f70d5c725376284b7007c6a0bb
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-03 13:21:51 -0500

    Definning instance attributes width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* Defining instance attributes w and h for Rectangle class
+*/
+
+class Rectangle {
+// Defining width and height attributes.
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
