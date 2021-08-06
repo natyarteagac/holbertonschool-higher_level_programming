bac38dc270d8e87f7d4483fac1a6ad9e572affc9
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-06 17:17:27 +0000

    Listing the table with the argv

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Create states table in hbtn_0e_0_usa with some data """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    # Connecting with database
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3],
+                         port=3306, charset="utf8")
+    # Creating the cursor
+    cur = db.cursor()
+    # Executing the Query
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(argv[4]))
+    # Selecting the information
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    # Closing cursor
+    cur.close()
+    # Closing database
+    db.close()
