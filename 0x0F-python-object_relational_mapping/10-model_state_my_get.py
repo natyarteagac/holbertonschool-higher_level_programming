75cacc6a6b2aae59c14456c5f08ba9e302cc404a
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-07 19:46:02 +0000

    Checking

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Script that lists all State objects from the database
+"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    arg_four = argv[4]
+    for state in session.query(State).order_by(State.id).all():
+        if state == arg_four:
+            print(len(state))
+    session.close()
