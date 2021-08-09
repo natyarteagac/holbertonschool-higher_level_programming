766e12fbd7ab5eb4cc1eae072c89bd505a36ca4a
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-09 13:45:19 +0000

    Fixixng changes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,4 +14,3 @@ class State(Base):
     id = Column(Integer, primary_key=True,
                 nullable=False)
     name = Column(String(128), nullable=False)
-    state = relationship("City", back_populates="cities")
