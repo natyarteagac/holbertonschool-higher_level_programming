ad85fceb346259e8bd37834fc1d55063ebf5caec
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-20 14:14:05 -0500

    Correct output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,12 @@
 from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         # Printing the type of the response
-        print("\t - type: {}".format(type(html)))
+        print("\t- type: {}".format(type(html)))
         # Printing the content.
-        print("\t - content: {}".format(html))
+        print("\t- content: {}".format(html))
         # Printing decode format
-        print("\t - utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
