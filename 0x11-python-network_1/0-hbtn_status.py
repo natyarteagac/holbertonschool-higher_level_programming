313898ff868c99c5724642359c936c7c48258b71
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-20 14:06:18 -0500

    Correct output

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Fetching https://intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        # Printing the type of the response
+        print("\t type: {}".format(type(html)))
+        # Printing the content.
+        print("\t content: {}".format(html))
+        # Printing decode format
+        print("\t utf8 content: {}".format(html.decode('utf-8')))
