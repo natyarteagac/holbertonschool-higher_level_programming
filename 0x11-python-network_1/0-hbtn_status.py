a397f934679daaa20c5067d0cc3fe70b7959faa9
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-20 14:11:51 -0500

    Checking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Fetching https://intranet.hbtn.io/status"""
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
