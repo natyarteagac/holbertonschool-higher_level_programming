e7f8e6e32c5052dc3ec01049fd78853810f7ef15
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-20 16:18:53 -0500

    Printing error status

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Displays the body of the response"""
+from sys import argv
+from urllib import request, error
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            html = response.read()
+            print("Index")
+    except error.HTTPError as exception:
+        print("Error code: {}".format(exception.read().decode('utf-8')[6:9]))
