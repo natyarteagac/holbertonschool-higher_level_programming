e6229a7be8c53ff5b7c496a19af8a3862474ab09
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-14 18:40:30 -0500

    Checking

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* Program to save into a new file an URL */
+
+const fs = require('fs');
+const { argv } = require('process');
+const request = require('request');
+const url = argv[2];
+const nameOfFile = argv[3];
+
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  fs.writeFile(nameOfFile, url, function (err) {
+    if (err) throw err;
+  })
+});
