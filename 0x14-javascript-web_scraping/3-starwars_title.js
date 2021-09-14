bce73ae0ab581be3f16122d3d93cacb97739b5d7
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-14 17:15:51 -0500

    Correct output

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 /* Program to write a string to a file */
 
-const args = process.argv;
+const { argv } = require('process');
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2]
 
-request('https://swapi-api.hbtn.io/api/films/' + args[2], function (error, body) {
-  if (error) {
-    console.error('code: %d', error);
-  } else {
-    console.log(JSON.parse(body.title));
-  }
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  console.log(JSON.parse(body).title);
 });
