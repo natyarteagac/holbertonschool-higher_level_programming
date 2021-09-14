d38e943a39fc2ccff10dc157daa3a1d02e750c2d
natyarteagac <natalia.arteagac94@gmail.com>
2021-09-14 18:22:38 -0500

    Correct output

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* Program to write a string to a file */
+
+const { argv } = require('process');
+const request = require('request');
+const url = argv[2];
+let counter = 0;
+
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  const urlJson = JSON.parse(body).results;
+  for (const movies of urlJson) {
+    const characters = movies.characters;
+    for (const oneChar of characters) {
+      if (oneChar.endsWith('18/') === true) {
+        counter += 1;
+      }
+    }
+  }
+  console.log(counter)
+});
